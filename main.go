cf031dfe64fef126c5bc05c9919749bc138f17d9
Rónán Duddy <work+github@ronanduddy.com>
2021-08-17 08:37:57 +0100

    Add a REPL

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"monkey/repl"
+	"os"
+	"os/user"
+)
+
+func main() {
+	user, err := user.Current()
+
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Monkey lang here %s\n", user.Username)
+
+	repl.Start(os.Stdin, os.Stdout)
+}
